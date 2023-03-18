82de03070e173e8ecaae898e8b5635c3bc3e2999
ik <doitdoik@naver.com>
2023-03-18 14:14:33 +0900

    create GalleryVO update mapper.xml

---
+++ deft_museum/src/main/java/com/ik/deft/dto/GalleryVO.java
@@ -0,0 +1,50 @@
+package com.ik.deft.dto;
+
+import lombok.Data;
+
+@Data
+public class GalleryVO {
+	private int idx;
+	private String name;
+	private String id;
+	private String pw;
+	private String src;
+	private String comment;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getSrc() {
+		return src;
+	}
+	public void setSrc(String src) {
+		this.src = src;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+}
